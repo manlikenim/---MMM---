8cdd0c227c9c1b06584cf69d979a40acb2a4c8ea
Nimesh Subedi <nimeshsubedi@gmail.com>
2017-12-23 18:03:40 +0000

    [NS] Populated location class with basic parameteres and corresponding methods.

--- src/Location.java
+++ src/Location.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Location {
+    private String name;
+    private String description;
+    private Alien currAlien;
+    private Item currItem;
+
+    public Location(String name, String description, Alien currAlien, Item currItem) {
+        this.name = name;
+        this.description = description;
+        this.currAlien = currAlien;
+        this.currItem = currItem;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Alien getCurrAlien() {
+        return currAlien;
+    }
+
+    public Item getCurrItem() {
+        return currItem;
+    }
+
+
 }
