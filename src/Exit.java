058413d416d4e1abf5f79b7710a7b4f54de54e55
Nimesh Subedi <nimeshsubedi@gmail.com>
2017-12-27 11:38:01 +0000

    [NS] Added Exit class to link the Locations in a more systematic way.

--- src/Exit.java
+++ src/Exit.java
@@ -1,4 +1,83 @@
-package PACKAGE_NAME;
-
 public class Exit {
+
+    // Numerical codes
+    public static final int UNDEFINED = 0;
+    public static final int FORWARD = 1;
+    public static final int BACKWARD = 2;
+    public static final int LEFT  = 3;
+    public static final int RIGHT  = 4;
+    public static final int IN = 5;
+    public static final int OUT = 6;
+
+    // String codes
+    public static final String[] dirName =
+            {
+                    "UNDEFINED",
+                    "FORWARD",
+                    "BACKWARD",
+                    "LEFT",
+                    "RIGHT",
+                    "IN",
+                    "OUT"
+            };
+
+    public static final String[] shortDirName =
+            {
+                    "NULL",
+                    "F",
+                    "B",
+                    "L",
+                    "R",
+                    "I",
+                    "O"
+            };
+
+    // Member variables
+    private int direction;
+    private Location leadsTo;
+
+    // Full name of direction
+    private String directionName;
+
+    // Shortened version of direction
+    private String shortDirectionName;
+
+
+    // Full constructor
+    public Exit( int direction, Location leadsTo )
+    {
+        //Assign direction
+        this.direction = direction;
+
+        // Assign location
+        this.leadsTo = leadsTo;
+
+        // Assign direction names
+        if (direction <= dirName.length )
+            this.directionName = dirName[direction];
+        if (direction <= shortDirName.length )
+            this.shortDirectionName = shortDirName[direction];
+
+    }
+
+    public Location getLeadsTo() {
+        return leadsTo;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public String getDirectionName() {
+        return directionName;
+    }
+
+    public String getShortDirectionName() {
+        return shortDirectionName;
+    }
+
+    @Override
+    public String toString() {
+        return this.directionName;
+    }
 }
--- src/Location.java
+++ src/Location.java
@@ -1,16 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Location {
     private String name;
     private String description;
     private Alien currAlien;
     private Item currItem;
+    private ArrayList<Exit> exits;
 
     public Location(String name, String description, Alien currAlien, Item currItem) {
         this.name = name;
         this.description = description;
         this.currAlien = currAlien;
         this.currItem = currItem;
+        this.exits = new ArrayList<>();
     }
 
+    // Getter methods for the parameters
     public String getName() {
         return name;
     }
@@ -27,5 +33,66 @@ public class Location {
         return currItem;
     }
 
+    public ArrayList<Exit> getExits() {
+        return exits;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    // Add new exit to the location
+    public void addExit(Exit newExit) {
+        exits.add(newExit);
+    }
+
+    // Remove an existing exit from the location
+    public void removeExit(Exit givenExit) {
+        if (exits.contains(givenExit)) {
+            exits.remove(givenExit);
+        }
+    }
+
+    // Show the details of the current location
+    public void showLocation() {
+        display("You are now in " + name);
+        display(description);
+        display("You can see an alien. It has a name tag on it. It reads " +
+                currAlien.getName());
+    }
+
+    // Show the details of the alien in the location
+    public void fleeOrFight(Player enemy) {
+        String choice = "INVALID";
+        String[] options = {"FIGHT", "FLEE"};
+        while (choice.equals("INVALID")) {
+            display("");
+            display("Do you want to fight " + currAlien.getName() + " or flee to " +
+                    "the next location?");
+            choice = Utils.getInput(options);
+
+        }
+        // given options to fight or flee
+        // if fight is chosen call attack option in alien
+        // if flee is chosen, go to the next location
+    }
+
+    // Start the fight between the alien and the player
+    private void beginFight(Player enemy) {
+        // Show a line saying the player attacks
+        // player attacks
+        // Show a line saying the alien attacks
+        // alien attacks
+    }
+
+    // Move on to the next location
+    private void flee(Player enemy) {
+        // Update the player's location
+    }
+
+    private void display(String message){
+        System.out.println(message);
+    }
 
 }
