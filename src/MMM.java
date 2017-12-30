fce11e87b88b9d4ca5cf1cb802632cf994d0a91d
Nimesh Subedi <nimeshsubedi@gmail.com>
2017-12-30 10:38:36 +0000

    [NS] Added an entry point for running the game.

---
+++ src/MMM.java
@@ -0,0 +1,8 @@
+public class MMM {
+
+    public static void main(String[] args) {
+        GameWorld gameWorld = new GameWorld();
+        gameWorld.chooseDifficulty();
+        gameWorld.startGame();
+    }
+}
